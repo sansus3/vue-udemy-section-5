951cc71250c882a274e3e3e9bb8a83cec64d7d80
webferrol <xurxo@webferrol.com>
2022-08-21 22:19:26 +0200

    Utilizada la Composition API de Pinia en vez de la Options API
    -destructuración
    -storeToRefs para varibles y computadas

--- src/stores/pokemon.js
+++ src/stores/pokemon.js
@@ -6,15 +6,21 @@ export const useStorePokemon = defineStore('pokemon',()=>{
     const favorites = ref([]);
     const router = useRouter();
 
+    if(window.localStorage.getItem('pokemon'))
+        favorites.value = JSON.parse(localStorage.getItem('pokemon'));
+
+
     const addFavorites = data => {        
         favorites.value.push(data);
+        window.localStorage.setItem('pokemon',JSON.stringify(favorites.value));
         router.push({name:'favorites'});
     }
 
     const isInFavorites = id => favorites.value.findIndex(item=>item.id===id)>-1;
 
-    const deleteFromFavorites = id => {
-        favorites.value = favorites.value.find(item=>item.id!==id);
+    const deleteFromFavorites = id => {        
+        favorites.value = favorites.value.filter(item=>item.id!==id);
+        window.localStorage.setItem('pokemon',JSON.stringify(favorites.value));
     }
 
     return {
